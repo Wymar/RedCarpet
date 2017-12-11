231231951790e4fb9781377cc592b4f3b3d3401a
Wymar Sane <wymarsane@gmail.com>
2017-12-11 13:15:57 +0100

    Adding missing files to project

--- app/src/androidTest/java/com/example/wymar/redcarpet/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wymar/redcarpet/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.wymar.redcarpet", appContext.getPackageName());
+        assertEquals("com.example.wymar.redcarpet2", appContext.getPackageName());
     }
 }
